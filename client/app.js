bfe0b2abea6a0aa4e80ba580507a5ddf09dc5a3a
Tara <TaraHussain97@gmail.com>
2020-11-16 12:50:09 +0000

    put reaction emojis inside reply div and worked on reply function js

--- client/app.js
+++ client/app.js
@@ -1,55 +1,4 @@
-// const btn = document.getElementById("loveButton");
-
-// const likes = document.getElementById("likesCounter");
-// const dislikes = document.getElementById("dislikesCounter");
-// const loves = document.getElementById("lovesCounter");
-
-// btn.addEventListener("click", onClick);
-
-// var clicks = 0;
-
-// function onClick() {
-
-//     clicks += 1;
-
-//     if(likes === )
-//   //   likes.innerHTML = clicks;
-
-//   //   dislikes.innerHTML = clicks;
-
-//   loves.innerHTML = clicks;
-// }
-
-// ********** SECOND TEST **********
-
-// "use strict";
-
-// // all the .button-counter from the DOM
-// const buttonsCounter = document.getElementsByClassName("button-counter");
-
-// // sets the content of the button to what value is currently set for data-counter
-// const buttonCounterSetText = (button) =>
-//   (button.innerHTML = `(${button.dataset.count || 0})`);
-
-// // handles click event on the button
-// const buttonCounterClickHandler = ({ currentTarget }) => {
-//   // increases the current value for data-count, or sets it to 0
-//   currentTarget.dataset.count =
-//     (parseInt(currentTarget.dataset.count) || 0) + 1;
-//   // see above
-//   buttonCounterSetText(currentTarget);
-// };
-
-// // initialize the buttons content and add listeners for click events
-// const buttonCounterInitialization = (button) => {
-//   buttonCounterSetText(button);
-//   button.addEventListener("click", buttonCounterClickHandler);
-// };
-
-// // initialize all buttons found with class .button-counter
-// [...buttonsCounter].forEach(buttonCounterInitialization);
-
-// ********** THIRD TEST **********
+// ********** emoji reaction function ********
 
 const button1 = document.getElementById("likes");
 const button2 = document.getElementById("dislikes");
@@ -76,11 +25,12 @@ button3.onclick = function () {
 
 // ********** Submit message function ********
 
-const form = document.querySelector("#post-form");
+const form = document.querySelector("#postForm");
 form.addEventListener("submit", submitPost);
 
-const formReply = document.querySelector("#replyForm");
+const formReply = document.querySelector("#postContainer");
 const postList = document.querySelector("#replyContent");
+const buttonContainer = document.querySelector("#buttonContainer");
 
 function submitPost(e) {
   e.preventDefault();
@@ -108,9 +58,13 @@ function appendPosts(data) {
 }
 
 function addToBody(postData) {
+  const replyButton = document.getElementById("replyButton");
   const newMessage = document.createElement("p");
   newMessage.textContent = `Anonymous says: ${postData.content}`;
   formReply.append(newMessage);
   formReply.style.visibility = "visible";
-  postList.style.visibility = "visible";
+  replyButton.style.visibility = "visible";
+  buttonContainer.style.visibility = "visible";
 }
+
+// postList.style.visibility = "visible";
