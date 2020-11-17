a7bc0435189976e87f2acae2221c5b8ae4aea446
stephanie-ai <stephanie_cooper@hotmail.com>
2020-11-17 16:14:24 +0000

    add functionality to dynamic reply button and reaction buttons

--- client/app.js
+++ client/app.js
@@ -1,59 +1,67 @@
 // ********** emoji reaction function ********
 
-const likeBtn = document.createElement("button");
-// const likeBtnCounter = document.createElement("p");
 
-const dislikeBtn = document.createElement("button");
-const loveBtn = document.createElement("button");
-
-// likeBtn.onclick = reactionCounter();
-
-// function reactionCounter() {
-//   let likecount = 0;
-//   return (likeBtn.textContent = "1 like");
-// return (likecount += 1);
+// function createElementButton(){
+// const likeBtn = document.createElement("button");
+// const dislikeBtn = document.createElement("button");
+// const loveBtn = document.createElement("button");
 // }
-let likecount = 0;
-let dislikecount = 0;
-let lovecount = 0;
 
-likeBtn.onclick = function (e) {
-  e.preventDefault();
-  likecount += 1;
-  likeBtn.innerHTML =  likecount;
-  console.log(likecount);
-  disable();
-};
 
-dislikeBtn.onclick = function (e) {
-  e.preventDefault();
-  dislikecount += 1;
-  dislikeBtn.innerHTML = dislikecount;
-  disable()
-};
+// let likecount = 0;
+// let dislikecount = 0;
+// let lovecount = 0;
 
-loveBtn.onclick = function (e) {
-  e.preventDefault();
-  lovecount += 1;
-  loveBtn.innerHTML = lovecount;
-  disable()
-};
+// likeBtn.onclick = function (e) {
+//   e.preventDefault();
+//   likecount += 1;
+//   likeBtn.innerHTML =  likecount;
+//   console.log(likecount);
+//   disable();
+// };
+
+// dislikeBtn.onclick = function (e) {
+//   e.preventDefault();
+//   dislikecount += 1;
+//   dislikeBtn.innerHTML = dislikecount;
+//   disable()
+// };
+
+// loveBtn.onclick = function (e) {
+//   e.preventDefault();
+//   lovecount += 1;
+//   loveBtn.innerHTML = lovecount;
+//   disable()
+// };
+
 
 //helper function to disable every other un-clicked button.
-function disable(){
-const buttons = [likeBtn, dislikeBtn, loveBtn];
-buttons.forEach(button => button.disabled = true)
+// function disable(){
+// const buttons = [likeBtn, dislikeBtn, loveBtn];
+// buttons.forEach(button => button.disabled = true)
+// }
+
+// ********** Get all previous posts ********
+getAllPosts();
+
+function getAllPosts() {
+  fetch("http://localhost:3012/anonymousPosts")
+    .then((r) => r.json())
+    .then(appendPosts)
+    .catch(console.warn);
 }
 
 // ********** Submit message function ********
 
 const form = document.querySelector("#postForm");
-form.addEventListener("submit", submitPost);
+form.addEventListener("submit", megaFunction);
 
 const formReply = document.querySelector("#postContainer");
 const postList = document.querySelector("#replyContent");
 const buttonContainer = document.querySelector("#buttonContainer");
-const replyButton = document.getElementById("replyButton");
+// const replyButton = document.getElementById("replyButton");
+
+
 
 function submitPost(e) {
   e.preventDefault();
@@ -68,7 +76,7 @@ function submitPost(e) {
   //   dislikes: e.target.dislikes.value,
   //   loves: e.target.loves.value,
   // }
-
+  
   createPost(postData);
 
   const options = {
@@ -81,20 +89,23 @@ function submitPost(e) {
 
   fetch("http://localhost:3012/anonymousPosts", options)
     .then((r) => r.json())
-    .then(addToBody)
+    // .then(createReactionButtons)
+    // .then(createReplyButton)
+    // debugger
+    // console.log('debugger working')
     .catch(console.warn);
 }
 
 function appendPosts(data) {
-  data.posts.forEach(addToBody);
+  data = JSON.parse(JSON.stringify(data));
+  data.forEach(createPost);
 }
 
-function addToBody() {
-  createReactionButtons();
-  createReplyButton();
-}
 
 function createReactionButtons() {
+  let likeBtn = document.createElement("button");
+  let dislikeBtn = document.createElement("button");
+  let loveBtn = document.createElement("button");
   const buttons = [likeBtn, dislikeBtn, loveBtn];
   buttonContainer.style.visibility = "visible";
   buttonContainer.append(likeBtn, dislikeBtn, loveBtn);
@@ -103,8 +114,8 @@ function createReactionButtons() {
   likeBtn.classList.add("like");
   dislikeBtn.classList.add("dislike");
   loveBtn.classList.add("love");
-
 }
+
 
 function createPost(postData) {
   const newPost = document.createElement("div");
@@ -114,10 +125,24 @@ function createPost(postData) {
   formReply.insertAdjacentElement("afterbegin", newPost);
   newPost.insertAdjacentElement("afterbegin", newMessage);
   formReply.style.visibility = "visible";
+  
 }
 
 function createReplyButton() {
-  replyButton.style.visibility = "visible";
+  const replyButton = document.createElement("button");
+  replyButton.textContent = "Reply";
+  // newPost.append(replyButton)
+  formReply.append(replyButton);
 }
+
+
+function megaFunction(e) {
+  submitPost(e);
+  createReactionButtons();
+  createReplyButton();
+  // createElementButton()
+}
+
+
 
 // postList.style.visibility = "visible";
