d7cfb070bdaecc2ba3fc7f78c38bc8b5a75786bc
Tara <TaraHussain97@gmail.com>
2020-11-18 18:10:37 +0000

    added reply key to object in json and creating reply functionality in app.js

--- server/createServer.js
+++ server/createServer.js
@@ -9,19 +9,28 @@ app.use(cors());
 
 const fs = require("fs");
 
-const data = fs.readFileSync('anonymousPosts.json');
-const anonymousPosts = JSON.parse(data);
+const dataPosts = fs.readFileSync("anonymousPosts.json");
+const anonymousPosts = JSON.parse(dataPosts);
+
+const dataReplies = fs.readFileSync("anonymousReplies.json");
+const anonymousReplies = JSON.parse(dataReplies);
 
 // const anonymousPosts = [
 //   { id: 1, content: "This is the first post", likes: 0, dislikes: 0, loves: 0 },
 // ];
 
 app.get("/", (req, res) => {
-  res.send("Hello tara!");
+  // res.send(anonymousPosts, anonymousReplies);
+  res.send("Hello La Star");
 });
 
 app.get("/anonymousPosts", (req, res) => {
   res.send(anonymousPosts);
+});
+
+app.get("/anonymousReplies", (req, res) => {
+  // res.send("connected to server");
+  res.send(anonymousReplies);
 });
 
 app.post("/anonymousPosts", (req, res) => {
@@ -30,6 +39,19 @@ app.post("/anonymousPosts", (req, res) => {
   const newPost = { id: newPostId, ...data };
   anonymousPosts.push(newPost);
   res.status(201).send(newPost);
+});
+
+app.post("/anonymousReplies", (req, res) => {
+  const data = req.body;
+  console.log(req.body);
+  // const newReplyId = anonymousPosts.length + 1;
+  //if postId==true
+  //   postId.reply.push(newReply)
+  // }
+  // const dataReceived
+  const newReply = { reply: dataReceived, ...data };
+  anonymousPosts.push(newReply);
+  res.status(201).send(newReply);
 });
 
 module.exports = app;
