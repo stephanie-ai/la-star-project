5daa3281faba75942275de526a5fabab80579289
stephanie-ai <stephanie_cooper@hotmail.com>
2020-11-24 21:33:50 +0000

    adjust port

--- server/startServer.js
+++ server/startServer.js
@@ -1,5 +1,5 @@
 const server = require("./createServer");
 
-server.listen(3012, () =>
-  console.log(`\nExpress departing now from port 3012!\n`)
-);
+const port = process.env.PORT || 3012; 
+
+server.listen(port, () => console.log(`Express is running on port ${port}`))
